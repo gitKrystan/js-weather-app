adf4071908ab9e1b6a9324aceec40dde4add656a
Kassidy Douglas and Krystan Menne <kmenne@gmail.com>
2016-02-16 14:11:50 -0800

    add interface for choosing temperature display type

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -2,18 +2,49 @@ var scripts = require('./../js/scripts.js');
 var apiKey = "cf92d9fbcc2eebdcaab0bf6fedff8e48";
 
 $(document).ready(function(){
+  var responseFromOWM;
   var $showWeather = $('#showWeather');
+  var chosenTemperatureDisplay = "fahrenheit";
+  $('input[type=radio][name=temperatureDisplayOptions][value=fahrenheit]')
+     .prop( "checked", true );
 
   $('#weatherLocation').click(function(){
     var city = $('#location').val();
     $('#location').val("");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      if(response.cod !== 200) {
-        $showWeather.text(response.message);
+      responseFromOWM = response;
+      if(responseFromOWM.cod !== 200) {
+        $showWeather.text(responseFromOWM.message);
       } else {
-        $showWeather.text("The humidity in " + city + " is " + response.main.humidity + "%");
-        $showWeather.append("<p>The temperature is " + convertKelvinToCelsius(response.main.temp) + '&deg;C</p>');
+        $showWeather.show();
+        $('#chosenCity').text(city);
+        $('#humidity').text(responseFromOWM.main.humidity);
+        changeTemperatureDisplay();
       }
     });
   });
+
+  $('input[type=radio][name=temperatureDisplayOptions]').change(function() {
+    chosenTemperatureDisplay = $(this).val();
+    changeTemperatureDisplay();
+  });
+
+  function changeTemperatureDisplay() {
+    var temperatureType = "";
+    var temperatureInteger;
+    if (responseFromOWM) {
+      if(chosenTemperatureDisplay === "celsius") {
+        temperatureInteger = convertKelvinToCelsius(responseFromOWM.main.temp);
+        temperatureType = "C";
+      } else if (chosenTemperatureDisplay === "kelvin") {
+        temperatureInteger = responseFromOWM.main.temp;
+        temperatureType = "K";
+      } else if (chosenTemperatureDisplay === "fahrenheit") {
+        temperatureInteger = convertKelvinToFahrenheit(responseFromOWM.main.temp);
+        temperatureType = "F";
+      }
+    }
+    $('#temperatureDisplay').text(temperatureInteger);
+    $('#temperatureType').text(temperatureType);
+  }
 });
